0cb7cb726bb047a2b267b2bbab1dcb13a9d8d268
Frank Matranga <thefrankmatranga@gmail.com>
2022-02-05 20:54:50 -0500

    Remove boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-		<h1>Wrathspriter?</h1>
+        <h1>Wrathspriter!</h1>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
