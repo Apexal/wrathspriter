81d4883e0d763ba5ae6273c125405e37a4e0c9a8
Charles Reverand <20057606+talios0@users.noreply.github.com>
2022-02-25 17:40:46 -0500

    Added basic inputs

--- src/pages/DevCreator/DevCreator.tsx
+++ src/pages/DevCreator/DevCreator.tsx
@@ -11,11 +11,49 @@ export function DevCreator() {
                 <p>Characters are saved as a JSON entry in IndexedDB, following the format from character.ts</p>
                 <br/>
                 <a className = "button is-responsive" href = "https://https://github.com/Apexal/wrathspriter/issues/24" target="_blank" rel="noreferrer" >
-                    Github Issue
+                    GitHub Issue
                 </a>
             </HelpButton>
             <header className = "container has-text-centered">
-                <h1 className = "is-uppercase is-size-1">Development Character Creator</h1>
+                <h1 className = "is-uppercase is-size-1 has-text-weight-bold">Development Character Creator</h1>
+                <h2 className = "is-size-5">
+                    Can you keep this page a secret?
+                </h2>
+                <div className = "field">
+                    <p className = "control">
+                        <input className = "input" type = "name" placeholder = "Name"></input>
+                    </p>
+                </div>
+                <div className = "control">
+                    <div className = "select">
+                        <select>
+                            <option selected>Major</option>
+                            <option>Computer Science</option>
+                            <option>Mathematics</option>
+                            <option>English</option>
+                            <option>Architecture</option>
+                            <option>Management</option>
+                            <option>Chemistry</option>
+                            <option>Physics</option>
+                            <option>Philosophy</option>
+                        </select>
+                    </div>
+                </div>
+                <div className = "control">
+                    <div className = "select">
+                        <select>
+                            <option selected>Minor</option>
+                            <option>Computer Science</option>
+                            <option>Mathematics</option>
+                            <option>English</option>
+                            <option>Architecture</option>
+                            <option>Management</option>
+                            <option>Chemistry</option>
+                            <option>Physics</option>
+                            <option>Philosophy</option>
+                        </select>
+                    </div>
+                </div>
             </header>
         </section>
     )
