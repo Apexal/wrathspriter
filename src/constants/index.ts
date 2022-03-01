3c6ed8564d9de7d87348f267f30e8b17eae03588
Frank Matranga <thefrankmatranga@gmail.com>
2022-03-01 10:56:49 -0500

    Create empty test character

---
+++ src/constants/index.ts
@@ -0,0 +1,23 @@
+import { Character } from "../interfaces";
+
+export const testCharacter: Character = {
+  name: "Frank",
+  backstory: "",
+  moveset: [],
+  major: {
+    name: "Computer Science",
+    backstory: "",
+    sampleMoveset: [],
+  },
+  minor: null,
+  hurtSoundEffects: [],
+  enterSoundEffects: [],
+  blockAnimation: [],
+  crouchAnimation: [],
+  walkAnimation: [],
+  dashAnimation: [],
+  grappledAnimation: [],
+  hurtAnimation: [],
+  winAnimation: [],
+  loseAnimation: [],
+};
