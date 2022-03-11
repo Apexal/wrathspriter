07f56ceef3c8e5ffa7e98c2b5b57d7c00a95f8d7
Frank Matranga <thefrankmatranga@gmail.com>
2022-03-11 14:34:54 -0500

    Add collider animation info

--- src/interfaces/animations.ts
+++ src/interfaces/animations.ts
@@ -1,9 +1,33 @@
+import { Vector2 } from ".";
+
 /** Represents a single frame of an animation. The image is Base64 encoded. */
 export interface AnimationFrame {
   /** The hardcoded pose for this animation frame move. Is set if this is a school program move. Meant to be displayed on top of camera for user to align themselves with. */
   base64EncodedPoseImage: string | null;
-  /** The actual frame image, Base64 encoded. */
+  /** The actual frame image, Base64 encoded */
   base64EncodedImage: string | null;
+  /** Animation details for hit collider */
+  hitCollider:
+    | {
+        /** Is this hit collider enabled on this frame and able to hurt a player? */
+        isEnabled: true;
+        /** The size of the hit collider on this frame */
+        size: Vector2;
+        /** The relative position on this frame of the hit collider to the player position */
+        position: Vector2;
+      }
+    | {
+        isEnabled: false;
+        size: null;
+        position: null;
+      };
+  /** Animation details for body collider */
+  bodyCollider: {
+    /** The size of the body collider on this frame */
+    size: Vector2;
+    /** The relative position on this frame of the body collider to the player position */
+    position: Vector2;
+  };
   /** The number of seconds this frame is displayed for before going to the next frame */
   durationInS: number;
 }
--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -2,3 +2,8 @@ export * from "./animations";
 export * from "./character";
 export * from "./action";
 export * from "./program";
+
+export interface Vector2 {
+  x: number;
+  y: number;
+}
