f4a0c668c8a87786ee43ad1c3879730deceed4f3
Frank Matranga <thefrankmatranga@gmail.com>
2022-03-27 17:26:26 -0400

    Implement working character details page

--- src/pages/CreateCharacterPage/CreateCharacterPage.tsx
+++ src/pages/CreateCharacterPage/CreateCharacterPage.tsx
@@ -4,6 +4,7 @@ import { Outlet } from "react-router-dom";
 import { emptyCharacter } from "../../constants";
 import { Character } from "../../interfaces";
 import { CharacterContext, CharacterContextType } from "../../state";
+import { downloadCharacter } from "../../utils/download";
 
 export function CreateCharacterPage() {
   const [character, setCharacter] = useState<Character>(emptyCharacter);
@@ -18,6 +19,19 @@ export function CreateCharacterPage() {
   return (
     <CharacterContext.Provider value={characterContextValue}>
       <Outlet />
+      <section className="section">
+        <div className="container">
+          <div className="buttons">
+            <button
+              className="button is-info"
+              onClick={() => downloadCharacter(character)}
+            >
+              Download
+            </button>
+          </div>
+        </div>
+      </section>
+      <hr />
     </CharacterContext.Provider>
   );
 }
