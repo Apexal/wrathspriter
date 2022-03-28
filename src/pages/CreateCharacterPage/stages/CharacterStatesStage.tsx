90f06b4361ff0fb5012365e0df6ceeaaa1a9ebd6
Frank Matranga <thefrankmatranga@gmail.com>
2022-03-27 23:40:07 -0400

    Start CharacterStateBox

--- src/pages/CreateCharacterPage/stages/CharacterStatesStage.tsx
+++ src/pages/CreateCharacterPage/stages/CharacterStatesStage.tsx
@@ -2,14 +2,37 @@ import { useContext } from "react";
 import { CharacterContext } from "../../../state";
 
 import states, { CharacterState } from "../../../constants/states";
+import { AnimatedSprite } from "../../../components/AnimatedSprite/AnimatedSprite";
 
 function CharacterStateBox({ state }: { state: CharacterState }) {
   const { character } = useContext(CharacterContext);
 
   return (
     <div className="box">
-      <h2 className="title is-capitalized">{state.id}</h2>
-      <h3 className="subtitle">{state.description}</h3>
+      <div className="columns">
+        <div className="column">
+          <h2 className="title is-capitalized">{state.id}</h2>
+          <h3 className="subtitle">{state.description}</h3>
+        </div>
+        <div className="column">
+          <h3 className="subtitle is-capitalized">Sound Effects</h3>
+        </div>
+        <div className="column">
+          <div className="animation-editor">
+            <div>
+              <h3 className="subtitle is-capitalized">Animation</h3>
+              <AnimatedSprite
+                width={150}
+                height={150}
+                animation={character.stateAnimations[state.id]}
+              />
+            </div>
+            <button className="button is-primary is-small">Add Frame</button>
+          </div>
+        </div>
+      </div>
+
+      <div className="sound-effects"></div>
     </div>
   );
 }
