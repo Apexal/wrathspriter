c19e86acb337672c7313403ad716f7f563b28dc8
alexisvh <avhernandez430@gmail.com>
2022-03-29 16:58:51 -0400

    editing bulma theme across pages

--- src/pages/ThemePage/ThemePage.tsx
+++ src/pages/ThemePage/ThemePage.tsx
@@ -29,20 +29,20 @@ export function ThemePage() {
 
       <section className={clsx("hero", "is-" + modifier)}>
         <div className="hero-body">
-          <p className="title">Theme Testing Page</p>
-          <p className="subtitle">Primary</p>
+          <p className="title">WRATHSKELLER</p>
+          <p className="subtitle">An RCOS Project</p>
         </div>
       </section>
       <section className="section is-text">
         <h1 className="title is-text">Page Title</h1>
         <h2 className="subtitle">Ooh cool subtitle...</h2>
 
-        <div className="notification is-danger">
+        <div className="notification is-info">
           Uh oh, something broke! Just kidding, this is for testing 👀
         </div>
 
         <div className="buttons">
-          <button className={clsx("button", "is-" + modifier)}>Continue</button>
+          <button className={clsx("button", "is-warning")}>Continue</button>
           <button className="button is-danger">Back</button>
           <button className="button">Back</button>
         </div>
