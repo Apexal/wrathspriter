b85eb8b988b1f080ca7506e39cc74d05f7caa0b0
Frank Matranga <thefrankmatranga@gmail.com>
2022-03-29 13:01:19 -0400

    Use environment variable for API base URL

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,5 @@
-const API_BASE_URL = "http://127.0.0.1:8000";
+const API_BASE_URL =
+  process.env.REACT_APP_API_BASE_URL ?? "http://127.0.0.1:8000";
 
 export async function processAudio(audioB64: string) {
   const response = await fetch(API_BASE_URL + "/audio", {
