17a62fa272094b9be9abd70bb1e1b9efcee8b534
Frank Matranga <thefrankmatranga@gmail.com>
2022-03-28 21:24:38 -0400

    Add more state description placeholders

--- src/constants/states.ts
+++ src/constants/states.ts
@@ -7,16 +7,40 @@ export type CharacterState = {
 
 const states: CharacterState[] = [
   {
+    id: "enter",
+    description: "Your character entering the stage at the start of the round.",
+  },
+  {
+    id: "win",
+    description: "",
+  },
+  {
+    id: "lose",
+    description: "",
+  },
+  {
     id: "idle",
     description: "Your character standing still between actions.",
+  },
+  {
+    id: "walk",
+    description: "Your character crouching.",
+  },
+  {
+    id: "jump",
+    description: "Your character crouching.",
   },
   {
     id: "crouch",
     description: "Your character crouching.",
   },
   {
-    id: "enter",
-    description: "Your character entering the stage at the start of the round.",
+    id: "block",
+    description: "Your character crouching.",
+  },
+  {
+    id: "hurt",
+    description: "Your character reacting to getting hit.",
   },
 ];
 
