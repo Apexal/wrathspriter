2c087cc55ab4a2d571d633813f6af6293a30468a
Frank Matranga <thefrankmatranga@gmail.com>
2022-03-28 22:40:20 -0400

    Process image on server

--- src/services/api.ts
+++ src/services/api.ts
@@ -14,3 +14,18 @@ export async function processAudio(audioB64: string) {
   const data = await response.json();
   return data.base64EncodedAudio;
 }
+
+export async function processImage(imageB64: string) {
+  const response = await fetch(API_BASE_URL + "/image", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      base64EncodedImage: imageB64,
+    }),
+  });
+
+  const data = await response.json();
+  return data.base64EncodedImage;
+}
