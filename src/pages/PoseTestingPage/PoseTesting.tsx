31a591e7e4effb05e733f4115460295838bd70ca
Frank Matranga <thefrankmatranga@gmail.com>
2022-04-09 11:24:56 -0400

    Update pose testing page

--- src/pages/PoseTestingPage/PoseTesting.tsx
+++ src/pages/PoseTestingPage/PoseTesting.tsx
@@ -1,19 +1,27 @@
 import { useRef } from "react";
-import { PoseCamera } from "../../components/PoseCamera/PoseCamera";
+import {
+  PoseCamera,
+  PoseCameraRef,
+} from "../../components/PoseCamera/PoseCamera";
 import { schoolPrograms } from "../../constants";
 
 export function PoseTestingPage() {
-  const videoRef = useRef<HTMLVideoElement>(null);
+  const poseCameraRef = useRef<PoseCameraRef | null>(null);
 
-  const handleCapture = () => {};
+  const handleCapture = () => {
+    if (poseCameraRef.current?.captureScreenshot) {
+      const screenshot = poseCameraRef.current?.captureScreenshot();
+      console.log(screenshot);
+    }
+  };
 
   return (
     <div className="section pose-testing-page">
       <div className="container">
         <PoseCamera
+          ref={poseCameraRef}
           pose={schoolPrograms[0].actionTemplates[0].animation[0].pose}
           isSkeletonDrawn={true}
-          ref={videoRef}
         />
         <button className="button" onClick={handleCapture}>
           Capture
