984c5577d79b1c41d0e903f6c2158d812ea60e28
Frank Matranga <thefrankmatranga@gmail.com>
2022-04-12 23:45:19 -0400

    Continuously take images

--- src/pages/CreateCharacterPage/stages/CharacterStatesStage.tsx
+++ src/pages/CreateCharacterPage/stages/CharacterStatesStage.tsx
@@ -11,13 +11,8 @@ import { defaultFrame } from "../../../constants";
 import "./CharacterStatesStage.scss";
 import clsx from "clsx";
 import { AudioRecorder } from "../../../components/AudioRecorder/AudioRecorder";
-import {
-  PoseCamera,
-  PoseCameraRef,
-} from "../../../components/PoseCamera/PoseCamera";
 import { NormalizedLandmarkList } from "@mediapipe/pose";
-import { useCountdown } from "../../../utils/hooks";
-import { checkIsFullyInFrame } from "../../../utils/posing";
+import { PoseCameraModal } from "../../../components/PoseCameraModal/PoseCameraModal";
 
 /** Editor for users to add, edit, and clear animation frames for a particular state. */
 function CharacterStateAnimationEditor({ state }: { state: CharacterState }) {
@@ -130,112 +125,15 @@ function CharacterStateAnimationEditor({ state }: { state: CharacterState }) {
     setCharacter(newCharacter);
   };
 
-  function PoseCameraModal({ isOpen }: { isOpen: boolean }) {
-    const poseCameraRef = useRef<PoseCameraRef | null>(null);
-    const [isFullyInFrame, setIsFullyInFrame] = useState<boolean>(false);
-    const [isWaitingToScreenshot, setIsWaitingToScreenshot] =
-      useState<boolean>(false);
-
-    const takeScreenshot = () => {
-      if (!poseCameraRef.current?.actualPose) return;
-
-      if (!checkIsFullyInFrame(poseCameraRef.current.actualPose)) {
-        window.alert("Your whole body was not in the frame!");
-        return;
-      }
-
-      const b64Url = poseCameraRef.current?.captureScreenshot();
-      if (!b64Url) return;
-
-      const b64 = b64Url.slice(b64Url.indexOf("base64,") + 7); // Remove URL prefix
-
-      handleProcessImage(b64, poseCameraRef.current?.actualPose ?? undefined);
-    };
-
-    const [isCountingDown, secondsLeft, startCountdown, endCountdown] =
-      useCountdown(5, takeScreenshot);
-
-    useEffect(() => {
-      if (isWaitingToScreenshot && isFullyInFrame) {
-        startCountdown();
-      } else if (isWaitingToScreenshot && !isFullyInFrame) {
-        endCountdown();
-      } else if (isCountingDown && !isFullyInFrame) {
-        endCountdown();
-      }
-    }, [
-      endCountdown,
-      isCountingDown,
-      isFullyInFrame,
-      isWaitingToScreenshot,
-      startCountdown,
-    ]);
-
-    return (
-      <div className={clsx("modal", isOpen && "is-active")}>
-        <div className="modal-background"></div>
-        <div className="modal-card">
-          <header className="modal-card-head">
-            <p className="modal-card-title">Time to Pose!</p>
-          </header>
-          <section className="modal-card-body">
-            <div className="columns is-vcentered">
-              <div className="column is-narrow">
-                <PoseCamera
-                  ref={poseCameraRef}
-                  isSkeletonDrawn={true}
-                  handleFullyInFrameChange={setIsFullyInFrame}
-                />
-              </div>
-              <div className="column has-text-centered">
-                {isCountingDown ? (
-                  <div>
-                    <span className="is-size-1">{secondsLeft}</span>
-                    <progress
-                      max={5}
-                      value={secondsLeft}
-                      className="progress is-small is-primary"
-                    />
-                  </div>
-                ) : isWaitingToScreenshot ? (
-                  <div>
-                    <p className="is-size-4">Get fully into the frame!</p>
-                    <progress className="progress is-primary" />
-                  </div>
-                ) : (
-                  <div>
-                    <p className="is-size-4 mb-5">
-                      Click to start countdown, then quick get into position!
-                    </p>
-                    <button
-                      className="button is-primary is-large"
-                      onClick={() => setIsWaitingToScreenshot(true)}
-                    >
-                      Start
-                    </button>
-                  </div>
-                )}
-              </div>
-            </div>
-          </section>
-          <footer className="modal-card-foot">
-            <button
-              className="button"
-              onClick={() => setIsPoseCameraModalOpen(false)}
-            >
-              Done
-            </button>
-          </footer>
-        </div>
-      </div>
-    );
-  }
-
   const frames = character.stateAnimations[state.id];
 
   return (
     <div className="box animation-editor">
-      <PoseCameraModal isOpen={isPoseCameraModalOpen} />
+      <PoseCameraModal
+        handleProcessImage={handleProcessImage}
+        isOpen={isPoseCameraModalOpen}
+        close={() => setIsPoseCameraModalOpen(false)}
+      />
       <h3 className="subtitle is-capitalized">Animation 🎞️</h3>
       {frames.length > 0 && (
         <div className="is-flex">
@@ -284,7 +182,7 @@ function CharacterStateAnimationEditor({ state }: { state: CharacterState }) {
             />
             <span className="file-cta">
               <span className="file-icon">📁</span>
-              <span className="file-label">Upload Images</span>
+              <span className="file-label">Upload Image</span>
             </span>
           </label>
         </div>
