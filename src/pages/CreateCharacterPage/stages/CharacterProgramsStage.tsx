fb9949598596fab9e8efc5d8a79e6cd58a5c8549
Frank Matranga <thefrankmatranga@gmail.com>
2022-04-14 18:11:27 -0400

    Expose ProgramCard component

--- src/pages/CreateCharacterPage/stages/CharacterProgramsStage.tsx
+++ src/pages/CreateCharacterPage/stages/CharacterProgramsStage.tsx
@@ -1,7 +1,107 @@
 import clsx from "clsx";
 import { useContext } from "react";
 import { schoolPrograms } from "../../../constants";
+import { Character, SchoolProgram } from "../../../interfaces";
 import { CharacterContext } from "../../../state";
+
+type ProgramCardPropTypes = {
+  program: SchoolProgram;
+  character?: Character;
+  setCharacter?: React.Dispatch<React.SetStateAction<Character>>;
+};
+export function ProgramCard({
+  program,
+  character,
+  setCharacter,
+}: ProgramCardPropTypes) {
+  return (
+    <div
+      key={program.id}
+      id={program.id}
+      className={clsx(
+        "card",
+        "mb-5",
+        character?.major?.id === program.id && "card-major-selected",
+        character?.minor?.id === program.id && "card-minor-selected"
+      )}
+    >
+      <div className="card-header">
+        <p className="card-header-title">{program.name}</p>
+        <div className="selected-text-container">
+          {character?.major?.id === program.id && (
+            <p className="card-header-title selected-text major-selected-text tag is-primary">
+              Major
+            </p>
+          )}
+          {character?.minor?.id === program.id && (
+            <p className="card-header-title selected-text minor-selected-text tag is-info">
+              Minor
+            </p>
+          )}
+        </div>
+      </div>
+
+      <div className="card-image">
+        <figure className="image">
+          <img src={program.coverImageURL ?? ""} alt="Cover for program" />
+        </figure>
+      </div>
+      <div className="card-content">
+        <div className="columns is-vcentered">
+          <div className="column">
+            <figure className="image is-128x128">
+              <img
+                src="https://media0.giphy.com/media/FopJy18z4t5hHlViZn/giphy.gif?cid=ecf05e478diube0770ny2jf9vye3fr4eefefgwvke6vk6ql3&rid=giphy.gif&ct=s"
+                alt="Placeholder"
+              />
+            </figure>
+
+            <div className="select">
+              <select>
+                {program.actionTemplates.map((template, index) => (
+                  <option key={index} value={index}>
+                    {template.name} ({template.type})
+                  </option>
+                ))}
+              </select>
+            </div>
+          </div>
+          <div className="column">
+            <em>
+              <p>"{program.backstory}"</p>
+            </em>
+          </div>
+        </div>
+      </div>
+      {setCharacter && character && (
+        <div className="card-footer">
+          <button
+            className="button card-footer-item"
+            onClick={() =>
+              setCharacter({
+                ...character,
+                major: program,
+              })
+            }
+          >
+            Choose Major
+          </button>
+          <button
+            className="button card-footer-item"
+            onClick={() =>
+              setCharacter({
+                ...character,
+                minor: program,
+              })
+            }
+          >
+            Choose Minor
+          </button>
+        </div>
+      )}
+    </div>
+  );
+}
 
 export function CharacterProgramsStage() {
   const { character, setCharacter } = useContext(CharacterContext);
@@ -29,92 +129,11 @@ export function CharacterProgramsStage() {
         <div className="columns">
           <div className="column is-half">
             {schoolPrograms.map((program) => (
-              <div
-                key={program.id}
-                id={program.id}
-                className={clsx(
-                  "card",
-                  "mb-5",
-                  character.major?.id === program.id && "card-major-selected",
-                  character.minor?.id === program.id && "card-minor-selected"
-                )}
-              >
-                <div className="card-header">
-                  <p className="card-header-title">{program.name}</p>
-                  <div className="selected-text-container">
-                    {character.major?.id === program.id && (
-                      <p className="card-header-title selected-text major-selected-text tag is-primary">
-                        Major
-                      </p>
-                    )}
-                    {character.minor?.id === program.id && (
-                      <p className="card-header-title selected-text minor-selected-text tag is-info">
-                        Minor
-                      </p>
-                    )}
-                  </div>
-                </div>
-
-                <div className="card-image">
-                  <figure className="image">
-                    <img
-                      src={program.coverImageURL ?? ""}
-                      alt="Cover for program"
-                    />
-                  </figure>
-                </div>
-                <div className="card-content">
-                  <div className="columns is-vcentered">
-                    <div className="column">
-                      <figure className="image is-128x128">
-                        <img
-                          src="https://media0.giphy.com/media/FopJy18z4t5hHlViZn/giphy.gif?cid=ecf05e478diube0770ny2jf9vye3fr4eefefgwvke6vk6ql3&rid=giphy.gif&ct=s"
-                          alt="Placeholder"
-                        />
-                      </figure>
-
-                      <div className="select">
-                        <select>
-                          {program.actionTemplates.map((template, index) => (
-                            <option key={index} value={index}>
-                              {template.name} ({template.type})
-                            </option>
-                          ))}
-                        </select>
-                      </div>
-                    </div>
-                    <div className="column">
-                      <em>
-                        <p>"{program.backstory}"</p>
-                      </em>
-                    </div>
-                  </div>
-                </div>
-                <div className="card-footer">
-                  <button
-                    className="button card-footer-item"
-                    onClick={() =>
-                      setCharacter({
-                        ...character,
-                        major: program,
-                      })
-                    }
-                  >
-                    Choose Major
-                  </button>
-                  <button
-                    className="button card-footer-item"
-                    onClick={() =>
-                      setCharacter({
-                        ...character,
-                        minor: program,
-                      })
-                    }
-                  >
-                    Choose Minor
-                  </button>
-                </div>
-              </div>
+              <ProgramCard
+                program={program}
+                character={character}
+                setCharacter={setCharacter}
+              />
             ))}
           </div>
           <div className="column is-half">
